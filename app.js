bf03d8668a5571d5ddef42268cd1bf5a875a9c75
Gri Leone <leonebarrionuevogrisel@gmail.com>
2023-10-30 18:23:04 -0300

    Git Update de Versiones

---
+++ app.js
@@ -0,0 +1,25 @@
+//Prueba//
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+app.get("/", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+})
+
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+
+app.get("/crearCuenta", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, "views/crearCuenta.html"));
+})
+
+app.listen(3030,  () => console.log("Servidor corriendo")
+);
+
